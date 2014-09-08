e80cb430cb70607a6dead33fa933852739adc2ff
Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
2014-09-08 12:17:33 +0200

    added README and timeout configuration

---
+++ README.md
@@ -0,0 +1,70 @@
+REDIS Controller for Revel.
+========
+
+Simple redis db controller for revel application.
+
+#### CONFIGURATION
+few variables are exposed for correct redis connection and authentication.
+each value expressed is the default configuration.
+```go
+redis.host=""
+// Redis host ip
+
+redis.port=6379
+// Redis tcp port
+
+redis.password=""
+// Redis password
+
+redis.trace=true
+// enable redis trace log on stderr usefull on deploy
+
+redis.check=true
+// Check idle connection after their usage with a simple PING command, on failure
+// close the application.
+
+redis.idle=10
+// Max idle connections for Poll, see: http://godoc.org/github.com/garyburd/redigo/redis#Pool.
+
+redis.timeout=240
+// Expressed in seconds, after this time the idle connection is closed.
+```
+#### Example production configuration
+```go
+redis.host=$REDIS_HOST
+redis.port=$REDIS_PORT
+redis.password=$REDIS_PASSWORD
+redis.trace=false
+redis.check=false
+redis.idle=3
+redis.timeout=60
+```
+#### USAGE
+just import the reredigo app package and add the reredigo controller to the application
+controller.
+For the redigo package documentation go to: http://godoc.org/github.com/garyburd/redigo/redis
+```go
+import (
+	"github.com/alex179ohm/reredigo/app"
+	"github.com/revel/revel"
+)
+
+type App struct {
+	*revel.Controller
+	reredis.RedisController
+}
+
+func (c App) Index() revel.Result {
+	connRedis = c.Pool.Get()
+	defer connRedis.Close()
+
+	connRedis.Do("SET", "foo", "bar")
+	bar, err := connRedis.Do("GET", "foo")
+	if err != nil {
+		revel.ERROR.Println(err)
+	}
+
+	c.RenderArgs["foo"] = bar
+	c.Render()
+}
+```
--- app/redis.go
+++ app/redis.go
@@ -1,104 +1,143 @@
 package reredis
 
 import (
-    "github.com/garyburd/redigo/redis";
-    "github.com/revel/revel";
-    "strings";
-    "time";
+	"github.com/agtorre/gocolorize"
+	"github.com/garyburd/redigo/redis"
+	"github.com/revel/revel"
+	"log"
+	"os"
+	"strings"
+	"time"
 )
 
 var (
-    pool *redis.Pool
+	pool *redis.Pool
 )
 
-func newPool(proto, server, password string, idle, active int) *redis.Pool {
-    if len(proto) == 0 { proto = "tcp" }
-    return &redis.Pool{
-        MaxIdle: idle,
-        MaxActive: active,
-        IdleTimeout: 240 * time.Second,
-        Dial: func () (redis.Conn, error) {
-            c, err := redis.Dial(proto, server)
-            if err != nil {
-                return nil, err
-            }
-            if len(password) > 0 {
-                if _, err := c.Do("AUTH", password); err != nil {
-                    c.Close()
-                    return nil, err
-                }
-            } else {
-                if _, err := c.Do("PING"); err != nil {
-                    c.Close()
-                    return nil, err
-                }
-            }
-            return c, err
-        },
-        TestOnBorrow: func(c redis.Conn, t time.Time) error {
-            if _, err := c.Do("PING"); err != nil {
-                return err
-            }
-            return nil
-        },
-    }
+func newPool(proto, server, password string,
+	idle, active, timeout int,
+	trace, check bool) *redis.Pool {
+	if len(proto) == 0 {
+		proto = "tcp"
+	}
+
+	redis_log := gocolorize.NewColor("magenta+b")
+	r := redis_log.Paint
+	redisLog := log.New(os.Stdout, r("REDIS "), log.Ldate|log.Ltime|log.Lshortfile)
+	return &redis.Pool{
+		MaxIdle:     idle,
+		MaxActive:   active,
+		IdleTimeout: timeout * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial(proto, server)
+			if trace {
+				c = redis.NewLoggingConn(c, redisLog, "")
+			}
+			if err != nil {
+				return nil, err
+			}
+			if len(password) > 0 {
+				if _, err := c.Do("AUTH", password); err != nil {
+					c.Close()
+					return nil, err
+				}
+			}
+			if check {
+				if _, err := c.Do("PING"); err != nil {
+					c.Close()
+					return nil, err
+				}
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if check {
+				if _, err := c.Do("PING"); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	}
 }
 
 func Init() {
-    // configurations
-    var (
-        found bool
-        redisUrl string
-        host string
-        port string
-        password string
-    )
+	// configurations
+	var (
+		found bool
+		//        redisUrl string
+		host     string
+		port     string
+		password string
+		idle     int
+		timeout  int
+		//		active int
+		trace bool
+		check bool
+	)
 
-    //TODO: include a regexp match and REDIS_URL env variable
+	//TODO: include a regexp match and REDIS_URL env variable
 
-    // set default configirations
+	// set default configurations
 
+	/*
+	   var redis_url = 0;
+	   //TODO: add unixUrl parser and check
+	   if redisUrl, found = revel.Config.String("redis.url"); !found {
+	       redisUrl = ""
+	       redis_url = 0;
+	       revel.INFO.Printf("Redis: redis")
+	   } else {
+	       redis_url = 1;
+	   }*/
+	//    if redis_url != 1 {
+	if host, found = revel.Config.String("redis.host"); !found {
+		host = ""
+		revel.INFO.Println("Redis: redis.host not found default is localhost")
+	}
+	if port, found = revel.Config.String("redis.port"); !found {
+		port = "6379"
+		revel.INFO.Println("Redis: redis.port not found default is 6379")
+	}
+	if password, found = revel.Config.String("redis.password"); !found {
+		password = ""
+		revel.INFO.Println("Redis: redis.password not found default is blank string")
+	}
+	//    }
 
-    var redis_url = 0;
-    //TODO: add unixUrl parser and check
-    if redisUrl, found = revel.Config.String("redis.url"); !found {
-        redisUrl = ""
-        redis_url = 1;
-        revel.INFO.Printf("Redis: redis")
-    } else {
-        redis_url = 0;
-    }
-    if redis_url != 1 {
-        if host, found = revel.Config.String("redis.host"); !found {
-            host = ""
-            revel.INFO.Printf("Redis: redis.host not found")
-        }
-        if port, found = revel.Config.String("redis.port"); !found {
-            port = "6379"
-            revel.INFO.Printf("Redis: redis.port not found")
-        }
-        if password, found = revel.Config.String("redis.password"); !found {
-            password = ""
-            revel.INFO.Printf("Redis: redis.password not found")
-        }
-    }
+	if idle, found = revel.Config.Int("redis.idle"); !found {
+		idle = 10
+		revel.INFO.Println("REDIS: redis.idle not found, default is 10")
+	}
+	if timeout, found = revel.Config("redis.timeout"); !found {
+		timeout = 240
+		revel.INFO.Println("REDIS: redis.timeout not found, default is 240")
+	}
+	if trace, found = revel.Config.Bool("redis.trace"); !found {
+		trace = true
+		revel.INFO.Println("REDIS: redis.trace not found default is true")
+	}
+	if check, found = revel.Config.Bool("redis.check"); !found {
+		check = true
+		revel.INFO.Println("REDIS: redis.check not found default is true")
+	}
 
-    url := []string{host, port}
+	url := []string{host, port}
 
-    pool = newPool("tcp", strings.Join(url, ":"), password, 3, 0)
+	pool = newPool("tcp", strings.Join(url, ":"), password, idle, 0, timeout, trace, check)
 }
 
 type RedisController struct {
-    *revel.Controller
-    Redis *redis.Pool
+	*revel.Controller
+	Pool *redis.Pool
 }
 
 func (c *RedisController) Begin() revel.Result {
-    c.Redis = pool
-    return nil
+	c.Pool = pool
+	return nil
 }
 
 func init() {
-    revel.OnAppStart(Init)
-    revel.InterceptMethod((*RedisController).Begin, revel.BEFORE)
+	revel.OnAppStart(Init)
+	revel.InterceptMethod((*RedisController).Begin, revel.BEFORE)
 }
